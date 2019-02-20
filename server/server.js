3ef9ce6b617a167ed0869fe7cad216970baf0f82
ecuyle <ecuyle@gmail.com>
2019-02-19 19:08:42 -0800

    Implement basic layout styling to appropriately render Reservation

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const morgan = require('morgan');
 const PORT = process.env.port || 3000;
 
 app.use(morgan('dev'));
-app.use(express.static('/client'));
+app.use('/', express.static(path.join(__dirname, '../client')));
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
