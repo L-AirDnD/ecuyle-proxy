12290ac0b87df9c60c5483bfbbd04612a49b35d1
ecuyle <ecuyle@gmail.com>
2019-02-22 10:18:13 -0800

    Refactor proxy to get id from window

--- server/server.js
+++ server/server.js
@@ -5,7 +5,10 @@ const morgan = require('morgan');
 const PORT = process.env.port || 3000;
 
 app.use(morgan('dev'));
-app.use('/', express.static(path.join(__dirname, '../client')));
+app.use(express.static(path.join(__dirname, '../client')));
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
